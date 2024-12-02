22204271e13a8f94860aa7d041eaedc0c1a01030
arthurlin <arthurlin0120@gmail.com>
2024-12-03 00:34:17 +0800

    finished

--- hw4.sh
+++ hw4.sh
@@ -3,4 +3,4 @@
 SPLIT_FOLDER=$1
 OUTPUT_FOLDER=$2
 
-python3 ./gaussian-splatting/infer.py -m ./gaussian-splatting/output/setting_1/ -s "$SPLIT_FOLDER" --output_folder "$OUTPUT_FOLDER" --skip_test
+python3 ./gaussian_splatting/infer.py -m ./gaussian_splatting/output/setting_1/ -s "$SPLIT_FOLDER" --output_folder "$OUTPUT_FOLDER" --skip_test
---
+++ hw4_download.sh
@@ -0,0 +1,1 @@
+wget -O ./gaussian_splatting/output/setting_1/point_cloud/iteration_100000/point_cloud.ply 'https://www.dropbox.com/scl/fi/ymnlaueyhbchpphofa99t/point_cloud.ply?rlkey=10r73h1435mamabfymkdlrjwm&st=89dqukhl&dl=1'
